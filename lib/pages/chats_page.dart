4827236cea981d5d039d51627cac2f066f83752a
monusk111 <mskakade111@gmail.com>
2023-01-29 18:33:36 +0530

    Reformatted chats page & added chats page provider

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -6,6 +6,7 @@ import 'package:safe_locations_application/widgets/custom_list_view_tiles.dart';
 
 //providers
 import '../provider/authentication_provider.dart';
+import '../provider/chats_page_provider.dart';
 
 //Widgets
 import '../widgets/top_bar.dart';
@@ -22,45 +23,61 @@ class _ChatsPageState extends State<ChatsPage> {
   late double _deviceWidth;
 
   late AuthenticationProvider _auth;
+  late ChatsPageProvider _pageProvider;
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
     _auth = Provider.of<AuthenticationProvider>(context);
-    return _buildUI();
-  }
-
-  Widget _buildUI() {
-    return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
-      height: _deviceHeight * 0.98,
-      width: _deviceWidth * 0.97,
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TopBar('Chats', primaryAction: IconButton(
-            icon: const Icon(
-              Icons.logout,
-              color: Color.fromRGBO(0, 82, 218, 1.0),
-            ),
-            onPressed: () {
-              _auth.logOut();
-            },
-          ),
-          ),
-          _chatsList(),
-        ],
-      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ChatsPageProvider>(
+          create: (_) => ChatsPageProvider(_auth),
+        ),
+      ],
+      child: _buildUI(),
     );
   }
 
+  Widget _buildUI() {
+    return Builder(
+      builder: (BuildContext _context) {
+        _pageProvider = _context.watch<ChatsPageProvider>();
+        return Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
+          height: _deviceHeight * 0.98,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TopBar(
+                'Chats',
+                primaryAction: IconButton(
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Color.fromRGBO(0, 82, 218, 1.0),
+                  ),
+                  onPressed: () {
+                    _auth.logOut();
+                  },
+                ),
+              ),
+              _chatsList(),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   Widget _chatsList() {
-    return Expanded(child: _chatTile(),);
+    return Expanded(
+      child: _chatTile(),
+    );
   }
 
   Widget _chatTile() {
@@ -73,5 +90,4 @@ class _ChatsPageState extends State<ChatsPage> {
         isActivity: false,
         onTap: () {});
   }
-
 }
---
+++ lib/provider/chats_page_provider.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+//packages
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//services
+import '../services/database_service.dart';
+
+//providers
+import '../provider/authentication_provider.dart';
+
+//models
+import '../models/chat.dart';
+import '../models/chat_message.dart';
+import '../models/chat_user.dart';
+
+class ChatsPageProvider extends ChangeNotifier {
+  AuthenticationProvider _auth;
+
+  late DatabaseService _db;
+
+  List<Chat>? chats;
+
+  late StreamSubscription _chatStream;
+
+  ChatsPageProvider(this._auth) {
+    _db = GetIt.instance.get<DatabaseService>();
+    getChats();
+  }
+
+  @override
+  void dispose() {
+    _chatStream.cancel();
+    super.dispose();
+  }
+
+  void getChats() async {
+
+  }
+
+}
