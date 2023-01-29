65639f3bac9af5610002cf30d8e6477e7080c4fe
monusk111 <mskakade111@gmail.com>
2023-01-29 19:08:12 +0530

    Received ChatStream from firebase

--- lib/provider/chats_page_provider.dart
+++ lib/provider/chats_page_provider.dart
@@ -37,7 +37,45 @@ class ChatsPageProvider extends ChangeNotifier {
   }
 
   void getChats() async {
+    try {
+      _chatStream =
+          _db.getChatsForUser(_auth.user.uid).listen((_snapshot) async {
+        chats = await Future.wait(_snapshot.docs.map((_d) async {
+          Map<String, dynamic> _chatData = _d.data() as Map<String, dynamic>;
 
+          //Get users in chat
+          List<ChatUser> _members = [];
+          for (var _uid in _chatData["members"]) {
+            DocumentSnapshot _userSnapshot = await _db.getUser(_uid);
+            Map<String, dynamic> _userData =
+                _userSnapshot.data() as Map<String, dynamic>;
+            _members.add(ChatUser.fromJSON(_userData));
+          }
+
+          //Get last message For chat
+          List<ChatMessage> _messages = [];
+          QuerySnapshot _chatMessage = await _db.getLastMessageForChat(_d.id);
+          if (_chatMessage.docs.isNotEmpty) {
+            Map<String, dynamic> _messageData =
+                _chatMessage.docs.first.data()! as Map<String, dynamic>;
+            ChatMessage _message = ChatMessage.fromJSON(_messageData);
+            _messages.add(_message);
+          }
+
+          //Return chat instance
+          return Chat(
+              uid: _d.id,
+              currentUserUid: _auth.user.uid,
+              messages: _messages,
+              members: _members,
+              activity: _chatData["is_activity"],
+              group: _chatData["is_group"]);
+        }).toList());
+        notifyListeners();
+      });
+    } catch (e) {
+      print("Error getting chats");
+      print(e);
+    }
   }
-
 }
