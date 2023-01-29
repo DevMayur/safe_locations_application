93eb3065b00de6a8a0754b4dd0d67ee61b9b1c67
monusk111 <mskakade111@gmail.com>
2023-01-29 16:03:06 +0530

    Added Chats page and users page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:safe_locations_application/pages/users_page.dart';
+import 'chats_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,6 +11,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+  final List<Widget> _pages = [
+    ChatsPage(),
+    UsersPage()
+  ];
   @override
   Widget build(BuildContext context) {
     return _buildUI();
@@ -16,8 +23,24 @@ class _HomePageState extends State<HomePage> {
 
   Widget _buildUI() {
     return Scaffold(
-      body: Container(
-        child: Text("HOME PAGE"),
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        onTap: (_index) {
+          setState(() {
+            _currentPage = _index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Chats',
+            icon: Icon(Icons.chat_bubble_sharp),
+          ),
+          BottomNavigationBarItem(
+            label: 'Users',
+            icon: Icon(Icons.supervised_user_circle_sharp),
+          )
+        ],
       ),
     );
   }
